f1068904eb720738e7b9df1ec131f735556542ba
satyaprakashpatel024 <satyaprakashpatel61@gmail.com>
2024-09-14 00:25:41 +0530

    repo updated

--- quicksort.cpp
+++ quicksort.cpp
@@ -1,4 +1,4 @@
-#include<bits\stdc++.h>
+#include <bits\stdc++.h>
 using namespace std;
 
 void swap(int *a, int *b)
@@ -8,66 +8,59 @@ void swap(int *a, int *b)
     *b = temp;
 }
 
-int partition(int arr[], int f , int l)
+int partition(int arr[], int f, int l)
 {
     int pivit = arr[l];
-    int i = (f-1);
+    int i = (f - 1);
 
-    for(int j=f; j<=l-1; j++)
+    for (int j = f; j <= l - 1; j++)
     {
-        if(arr[j]<pivit)
+        if (arr[j] < pivit)
         {
             i++;
-            swap(&arr[i],&arr[j]);
-            /*int temp = arr[i];
-            arr[i] = arr[j];
-            arr[j] = temp;*/
+            swap(&arr[i], &arr[j]);
         }
     }
-    swap(&arr[i+1],&arr[l]);
-    /*int temp = arr[i+1];
-    arr[i+1] = arr[l];
-    arr[l] = temp;*/
-    return i+1;
+    swap(&arr[i + 1], &arr[l]);
+    return i + 1;
 }
 
-void quicksort(int arr[],int f, int l)
+void quicksort(int arr[], int f, int l)
 {
-    if(f>=l)
+    if (f >= l)
     {
         return;
     }
-        int loc = partition(arr,f,l);
-        quicksort(arr,f,loc-1);
-        quicksort(arr,loc+1,l);
-    
+    int loc = partition(arr, f, l);
+    quicksort(arr, f, loc - 1);
+    quicksort(arr, loc + 1, l);
 }
 
 void printarr(int arr[], int n)
 {
-    
-    cout<<endl;
-    for(int i=0; i<n; i++)
+
+    cout << endl;
+    for (int i = 0; i < n; i++)
     {
-        cout<<arr[i]<<" ";
+        cout << arr[i] << " ";
     }
-    cout<<endl;
-    cout<<endl;
+    cout << endl;
+    cout << endl;
 }
 int main()
 {
-    int arr[] = {11,18,9,13,10,12,15,14};
-    int n = sizeof(arr)/sizeof(int);
-     cout<<"\n NAME : SATYA PRAKASH PATEL \n";
-    cout<<" BRANCH : CSE(B2) \n";
-    cout<<" ENROLLMENT NO. : 0131CS211099 \n";
-    cout<<"\n Topic : Quick sort \n";
-    cout<<"\n Printing unsorted array : \n";
-    printarr(arr,n);
+    int arr[] = {11, 18, 9, 13, 10, 12, 15, 14};
+    int n = sizeof(arr) / sizeof(int);
+    cout << "\n NAME : SATYA PRAKASH PATEL \n";
+    cout << " BRANCH : CSE(B2) \n";
+    cout << " ENROLLMENT NO. : 0131CS211099 \n";
+    cout << "\n Topic : Quick sort \n";
+    cout << "\n Printing unsorted array : \n";
+    printarr(arr, n);
 
-    quicksort(arr,0,n-1);
-    cout<<" Printing  sorted  array : \n";
-    printarr(arr,n);
-    cout<<endl;
+    quicksort(arr, 0, n - 1);
+    cout << " Printing  sorted  array : \n";
+    printarr(arr, n);
+    cout << endl;
     return 0;
 }
